1101fcab2adbbb6d0b0558d1ff81c573bed03792
nagenn <44678680+nagenn@users.noreply.github.com>
2020-09-24 16:16:31 +0530

    fixed a metamask connection issue by enabling ethereum

--- src/js/app.js
+++ src/js/app.js
@@ -14,10 +14,12 @@ App = {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
+      ethereum.enable();
     } else {
       // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
+      ethereum.enable();
     }
     return App.initContract();
   },
